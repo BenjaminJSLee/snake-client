f1e6cd8e89aa4da956b90c96ff9e389f73cf7a10
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-04 22:57:26 +0000

    re-added handleUserInput function to the global scope in input.js

--- input.js
+++ input.js
@@ -1,27 +1,28 @@
 let connection;
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log("exited");
+    process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  } else if (key === ' ') {
+    connection.write(`Say: Sss`);
+  }
+};
+
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  const handleUserInput = (key) => {
-    if (key === '\u0003') {
-      console.log("exited");
-      process.exit();
-    } else if (key === 'w') {
-      connection.write("Move: up");
-    } else if (key === 'a') {
-      connection.write("Move: left");
-    } else if (key === 's') {
-      connection.write("Move: down");
-    } else if (key === 'd') {
-      connection.write("Move: right");
-    } else if (key === ' ') {
-      connection.write(`Say: Sss`);
-    }
-  };
   stdin.on('data', handleUserInput);
   return stdin;
 };
