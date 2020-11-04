40881ea014ab68191c22d8eef0b0179bdf63c5b1
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-04 21:51:04 +0000

    added a stdin handler for keyboard input in play.js

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
 console.log('Connecting ...');
 connect();
+
+const stdin = setupInput();
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log("OOPSIE WOOPSIE");
+    process.exit();
+  }
+};
+
+stdin.on('data', handleUserInput);
