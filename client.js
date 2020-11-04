f0ae14ffe321c7917433100297b322ac9d44dd3d
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-04 21:33:52 +0000

    added an 'on connection' message as well as a write to the server giving name info (client.js)

--- client.js
+++ client.js
@@ -10,6 +10,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server.");
+    conn.write("Name: BEN");
+  });
   conn.on('data', (data) => {
     process.stdout.write(data);
   });
